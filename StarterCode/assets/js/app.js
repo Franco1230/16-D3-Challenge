5a92965441b6d4f67853d058a1543a46f655411b
Franco1230 <franco.hfh1230@gmail.com>
2021-11-20 13:44:08 +1100

    test

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -6,9 +6,9 @@ var chosenYAxis = "healthcare";
 function xScale(data, chosenXAxis, chartWidth) {
 	// Create scales
 	var xLinearScale = d3.scaleLinear()
-						 .domain([d3.min(data, d => d[chosenXAxis]) * .8,
-            d3.max(data, d => d[chosenXAxis]) * 1.1])
-			  .range([0, chartWidth]);
+						 .domain([d3.min(data, d => d[chosenXAxis]) * 0.8,
+            		   d3.max(data, d => d[chosenXAxis]) * 1.1])
+			   			 .range([0, chartWidth]);
 return xLinearScale;
 }
 
@@ -20,23 +20,26 @@ function renderXAxes(newXScale, xAxis) {
 		 .call(bottomAxis);
 return xAxis;
 }
+
 // Function used for updating y-scale var upon click on axis label.
 function yScale(data, chosenYAxis, chartHeight) {
-    // Create scales.
-    var yLinearScale = d3.scaleLinear()
-        .domain([d3.min(data, d => d[chosenYAxis]) * .8,
-            d3.max(data, d => d[chosenYAxis]) * 1.2])
-        .range([chartHeight, 0]);
-    return yLinearScale;
+	// Create scales
+	var yLinearScale = d3.scaleLinear()
+						 .domain([d3.min(data, d => d[chosenYAxis]) * 0.8,
+			           d3.max(data, d => d[chosenYAxis]) * 1.2])
+        				 .range([chartHeight, 0]);
+return yLinearScale;
 }
+
 // Function used for updating yAxis var upon click on axis label.
 function renderYAxes(newYScale, yAxis) {
-    var leftAxis = d3.axisLeft(newYScale);
-    yAxis.transition()
-        .duration(1000)
-        .call(leftAxis);
-    return yAxis;
+	var leftAxis = d3.axisLeft(newYScale);
+	yAxis.transition()
+		 .duration(1000)
+         .call(leftAxis);
+return yAxis;
 }
+
 // Function used for updating circles group with a transition to new circles.
 function renderCircles(circlesGroup, newXScale, newYScale, chosenXAxis, chosenYAxis) {
     circlesGroup.transition()
@@ -45,61 +48,65 @@ function renderCircles(circlesGroup, newXScale, newYScale, chosenXAxis, chosenYA
         .attr("cy", d => newYScale(d[chosenYAxis]));
     return circlesGroup;
 }
+
 // Function used for updating text in circles group with a transition to new text.
 function renderText(circletextGroup, newXScale, newYScale, chosenXAxis, chosenYAxis) {
     circletextGroup.transition()
-        .duration(1000)
-        .attr("x", d => newXScale(d[chosenXAxis]))
-        .attr("y", d => newYScale(d[chosenYAxis]));
-    return circletextGroup;
+    		       .duration(1000)
+				   .attr("x", d => newXScale(d[chosenXAxis]))
+				   .attr("y", d => newYScale(d[chosenYAxis]));
+return circletextGroup;
 }
+
 // Function used for updating circles group with new tooltip.
 function updateToolTip(chosenXAxis, chosenYAxis, circlesGroup, textGroup) {
     // Conditional for X Axis.
     if (chosenXAxis === "poverty") {
-        var xlabel = "Poverty: ";
-    } else if (chosenXAxis === "income") {
-        var xlabel = "Median Income: "
-    } else {
-        var xlabel = "Age: "
+		var xlabel = "Poverty: ";
+	}
+	else if (chosenXAxis === "income") {
+		var xlabel = "Median Income: "
     }
+	else {var xlabel = "Age: "}
+
     // Conditional for Y Axis.
     if (chosenYAxis === "healthcare") {
-        var ylabel = "Lacks Healthcare: ";
-    } else if (chosenYAxis === "smokes") {
-        var ylabel = "Smokers: "
-    } else {
-        var ylabel = "Obesity: "
+		var ylabel = "Lacks Healthcare: ";
     }
+	else if (chosenYAxis === "smokes") {
+		var ylabel = "Smokers: "
+    }
+	else {var ylabel = "Obesity: "}
+
     // Define tooltip.
     var toolTip = d3.tip()
-        .offset([120, -60])
-        .attr("class", "d3-tip")
-        .html(function(d) {
-            if (chosenXAxis === "age") {
-                // All yAxis tooltip labels presented and formated as %.
-                // Display Age without format for xAxis.
-                return (`${d.state}<hr>${xlabel} ${d[chosenXAxis]}<br>${ylabel}${d[chosenYAxis]}%`);
-                } else if (chosenXAxis !== "poverty" && chosenXAxis !== "age") {
-                // Display Income in dollars for xAxis.
-                return (`${d.state}<hr>${xlabel}$${d[chosenXAxis]}<br>${ylabel}${d[chosenYAxis]}%`);
-                } else {
-                // Display Poverty as percentage for xAxis.
-                return (`${d.state}<hr>${xlabel}${d[chosenXAxis]}%<br>${ylabel}${d[chosenYAxis]}%`);
-                }      
-        });
+					.offset([120, -60])
+					.attr("class", "d3-tip")
+					.html(function(d) {
+						if (chosenXAxis === "age") {
+							// All yAxis tooltip labels presented and formated as %.
+							// Display Age without format for xAxis.
+							return (`${d.state}<hr>${xlabel} ${d[chosenXAxis]}<br>${ylabel}${d[chosenYAxis]}%`);
+						}
+						else if (chosenXAxis !== "poverty" && chosenXAxis !== "age") {
+							// Display Income in dollars for xAxis.
+							return (`${d.state}<hr>${xlabel}$${d[chosenXAxis]}<br>${ylabel}${d[chosenYAxis]}%`);
+						}
+						else {
+							// Display Poverty as percentage for xAxis.
+							return (`${d.state}<hr>${xlabel}${d[chosenXAxis]}%<br>${ylabel}${d[chosenYAxis]}%`);
+						}
+					});
     circlesGroup.call(toolTip);
     // Create "mouseover" event listener to display tool tip.
-    circlesGroup
-        .on("mouseover", function(data) {
-            toolTip.show(data, this);
-        })
-        .on("mouseout", function(data) {
-            toolTip.hide(data);
-        });
-    textGroup
-        .on("mouseover", function(data) {
-            toolTip.show(data, this);
+	circlesGroup.on("mouseover", function(data) {
+		toolTip.show(data, this);
+	})
+				.on("mouseout", function(data) {
+					toolTip.hide(data);
+				});
+    textGroup.on("mouseover", function(data) {
+		toolTip.show(data, this);
         })
         .on("mouseout", function(data) {
             toolTip.hide(data);
